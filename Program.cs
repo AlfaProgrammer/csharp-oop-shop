9785436794df07313322119d963db37705da3877
AlfaProgrammer <alexloghin@outlook.it>
2022-06-29 15:33:13 +0200

    Arrivato fino a stampa prezzo + iva

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using csharp_oop_shop.Shop;
+
+Product prod1 = new Product("HP Laptop", "Bellissimo", 1252.26);
+Console.WriteLine($"prod1 code: {prod1.Code}");
+Product prod2 = new Product("Mac Book", "Fantastico", 2152.99);
+Console.WriteLine($"prod2 code: {prod2.Code}");
+Product prod3 = new Product("Pera Book", "Deforme", 999.86);
+Console.WriteLine($"prod3 code: {prod3.Code}");
+
+Console.WriteLine();
+
+Console.WriteLine("TEST TUTTI I METHODS");
+Console.WriteLine($"{prod2.Price} EUR");
+Console.WriteLine($"prod2 prezzoiva: {prod2.getPriceWithVat()} EUR");
+Console.WriteLine($"stato dell'index dopo la creazione dell'ultima istanza: {Product.CodeIndex}");
+
+/*
+Console.WriteLine(prod1.name);
+Console.WriteLine(prod1.description);
+Console.WriteLine(prod1.Price);
+Console.WriteLine(prod1.Iva); */
---
+++ Shop/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop.Shop
+{
+    public class Product
+    {
+        public Product(string name, string description, double price) {
+            this.name = name;
+            this.description = description;
+            this._price = price;
+            //assegnazione index al code dell'istanza
+            this._code = _codeIndex;
+            // incremento index statico
+            _codeIndex++;
+        }
+
+        private static int _codeIndex = 0; // è un index di riferimento che deve stare solo nella classe 
+        private int _code;
+        public string name;
+        public string description;
+        private double _price;
+        private double _vat = 20;
+
+        public double Price {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+
+        public double Vat { 
+            get { return this._vat; }
+            set { this._vat = value; }
+        }
+
+        public int Code {
+            get { return this._code; }
+        }
+
+        public static int CodeIndex {
+            get { return Product._codeIndex; }
+        }
+
+        public double getPriceWithVat() { 
+            return Math.Round(this._price + (this._price * this._vat / 100), 2);
+        }
+    }
+}
